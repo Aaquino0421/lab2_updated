babf665b76dabe983432f89309162abb8a211055
Aaquino21 <adaquino@student.apc.edu.ph>
2019-09-24 15:25:12 +0800

    no message

--- activities.php
+++ activities.php
@@ -87,12 +87,12 @@
           <div class="col-lg-4 col-md-6">
             <div class="journal-info mb-30">
 
-              <a href="act1.html"><img src="images/alex19.jpeg" height = 300 width = 270 class="img-responsive" alt="img"></a>
+              <a href="act1.html"><img src="images/alex19.jpg" height = 300 width = 270 class="img-responsive" alt="img"></a>
 
               <div class="journal-txt">
 
                 <h4><a href="act1.html">SELF INTRODUCTION</a></h4>
-                <p class="separator">Hi! I'm Alexis Aquino but you can call me Alex. I'm 19 years old and I live at 820 I. Santos St. Malibay, Pasay City. I graduated senior high...
+                <p class="separator">Hi! I'm Alexis Aquino but you can call me Alex. I'm 19 years old and I live at 820 I. Santos St. Malibay, Pasay City. I graduated senior high school from Sta. Clara Parish School. I’m taking Bachelor of Science in Information Technology - specialized in Mobile Internet in Asia Pacific College. I am a member in APC Dance Company and Junior Philippine Computer Society – APC.
                 </p>
               </div>
 
@@ -102,12 +102,12 @@
           <div class="col-lg-4 col-md-6">
             <div class="journal-info mb-30">
 
-              <a href="act2.html"><img src="images/kim.png" class="img-responsive" alt="img"></a>
+              <a href="act2.html"><img src="images/kim.png" height = 300 width = 270 class="img-responsive" alt="img"></a>
 
               <div class="journal-txt">
 
                 <h4><a href="act2.html">FAVORITE APPLICATION</a></h4>
-                <p class="separator"> I have this game in my phone that I always play when I'm bored or when I have free time. This game is...
+                <p class="separator"> I have this game in my phone that I always play when I’m bored or when I have free time. This game is free to play or you can choose to pay real money for extra items. Internet/mobile data is required for you to play. The player’s goal is to level up their fame and reputation, starting on the E-list and rising to A-list by gaining fans.  
                 </p>
 
               </div>
@@ -118,12 +118,12 @@
           <div class="col-lg-4 col-md-6">
             <div class="journal-info mb-30">
 
-              <a href="act3.html"><img src="images/elev.jpg" height = 300 width = 270 class="img-responsive" alt="img"></a>
+              <a href="act3.html"><img src="images/elev.jpg" height = 300 width = 270  class="img-responsive" alt="img"></a>
 
               <div class="journal-txt">
 
                 <h4><a href="act3.html">1000 FLOOR ELEVATOR</a></h4>
-                <p class="separator">Assuming that designing a thousand floor elevator will be a hard job to do. It will consume time for everyone to get...
+                <p class="separator">Assuming that designing a thousand floor elevator will be a hard job to do. It will consume time for everyone to get to their floor, if there is only a single-decker elevator. Let’s divide the elevator by 10 and each of it has 100 floors. 
                 </p>
 
               </div>
@@ -134,12 +134,12 @@
           <div class="col-lg-4 col-md-6">
             <div class="journal-info mb-30">
 
-              <a href="act4.html"><img src="images/need.png" height = 300 width = 270 class="img-responsive" alt="img"></a>
+              <a href="act4.html"><img src="images/need.jpg" height = 300 width = 270  class="img-responsive" alt="img"></a>
 
               <div class="journal-txt">
 
                 <h4><a href="act4.html">NEED FINDING</a></h4>
-                <p class="separator">In the current status quo of our society, people have the capability to think of travelling on places they... 
+                <p class="separator">In the current status quo of our society, people have the capability to think of travelling on places they haven`t visited yet. They are enticed with the beautiful views of nature along with its different activities and events to begin with. People are always looking forward on creating unforgettable moments, experiences and memories with or without their love ones.
                 </p>
 
               </div>
@@ -150,12 +150,12 @@
           <div class="col-lg-4 col-md-6">
             <div class="journal-info mb-30">
 
-              <a href="act5.html"><img src="images/login.jpg" height = 300 width = 300 class="img-responsive" alt="img"></a>
+              <a href="act5.html"><img src="images/travel.JPG" height = 300 width = 270 class="img-responsive" alt="img"></a>
 
               <div class="journal-txt">
 
                 <h4><a href="act5.html">INITIAL PROTOTYPE - LOG IN PAGE</a></h4>
-                <p class="separator">The initial prototype of the log in page of our Travel Mo To App.
+                <p class="separator">The initial prototype of the log in page of our Eye Clinic.
                 </p>
 
               </div>
@@ -171,7 +171,7 @@
               <div class="journal-txt">
 
                 <h4><a href="act6.html">PERSONA</a></h4>
-                <p class="separator">Stated here is my persona where I created which can be used on our project - Travel Mo To App.
+                <p class="separator">Stated here is my persona where I created which can be used on our project - Travel Mo To.
                 </p>
 
               </div>
@@ -187,16 +187,16 @@
               <div class="journal-txt">
 
                 <h4><a href="act7.html">INFORMATION DESIGN</a></h4>
-                <p class="separator">The challenge here is to re-design a certain invitation given by our USERDES professor.
+                <p class="separator">The challenge here is to re-design a certain invitation given by our userdes professor.
                 </p>
               </div>
             </div>
           </div>
 
- 		<div class="col-lg-4 col-md-6">
+    <div class="col-lg-4 col-md-6">
             <div class="journal-info mb-30">
 
-              <a href="act8.html"><img src="images/grid.png" height = 300 width = 300 class="img-responsive" alt="img"></a>
+              <a href="act8.html"><img src="images/grid.png" height = 300 width = 270 class="img-responsive" alt="img"></a>
 
               <div class="journal-txt">
 
@@ -219,9 +219,12 @@
       <div class="socials-media text-center">
 
         <ul class="list-unstyled">
-          <li><a href="https://www.facebook.com/alexaquino21"><i class="ion-social-facebook"></i></a></li>
-          <li><a href="https://twitter.com/Aaquino0421"><i class="ion-social-twitter"></i></a></li>
-          <li><a href="https://www.instagram.com/lxsqn"><i class="ion-social-instagram"></i></a></li>
+          <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+          <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+          <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+          <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+          <li><a href="#"><i class="ion-social-tumblr"></i></a></li>
+          <li><a href="#"><i class="ion-social-dribbble"></i></a></li>
         </ul>
 
       </div>
